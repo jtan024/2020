72d471395a4fe38e0b949b8ff7acc3512c19e3dd
Jerry Tan <jerry88tan@gmail.com>
2019-10-06 00:28:13 -0700

    add icons and faq

--- RoseHack2020/src/components/layout.js
+++ RoseHack2020/src/components/layout.js
@@ -8,9 +8,15 @@
 import React from "react"
 import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
+import { FaFacebookF } from 'react-icons/fa';
+import { FaTwitter } from 'react-icons/fa';
+import {FaLinkedinIn} from 'react-icons/fa';
+import {FaInstagram} from 'react-icons/fa';
+
 
 import Header from "./header"
 import "./layout.css"
+import "../styles/global.css"
 
 const Layout = ({ children }) => {
   const data = useStaticQuery(graphql`
@@ -28,9 +34,19 @@ const Layout = ({ children }) => {
       <div>
         <main>{children}</main>
         <footer style={{textAlign : "center"}}>
+          <div className={'social row-item'}>
+          <a href={'https://www.facebook.com/rosehackucr/'} target={'_blank'}><h2 style={{color: "white", marginTop:'3px'}}> <FaFacebookF /> </h2></a>
+          <a href={'https://twitter.com/rosehack_ucr'} target={'_blank'}><h2 style={{color: "white", marginTop:'3px'}}> <FaTwitter /> </h2></a>
+          <a href={'https://www.linkedin.com/company/rose-hack/'} target={'_blank'}><h2 style={{color: "white", marginTop:'3px'}}> <FaLinkedinIn /> </h2></a>
+          <a href={'https://www.instagram.com/rosehackucr/'} target={'_blank'}><h2 style={{color: "white", marginTop:'3px'}}> <FaInstagram /> </h2></a>
+          </div>
           Built with ❤️
           {` `}
           in Riverside, California
+
+
+
+
         </footer>
       </div>
     </>
--- RoseHack2020/src/components/question.js
+++ RoseHack2020/src/components/question.js
@@ -15,37 +15,62 @@ export default class Question extends Component {
     this.handleClick = this.handleClick.bind(this);
 
   }
+
   handleClick() {
     this.setState(state => ({
       isToggleOn: !state.isToggleOn
     }));
   }
+  toggleBox = () => {
+    this.setState(prevState => ({ isBoxVisible: !prevState.isBoxVisible }));
+  };
 
-  render() {
-    return (
-      <Flippy
-        flipOnHover={false} // default false
-        flipOnClick={true} // default false
-        flipDirection="horizontal" // horizontal or vertical
-        ref={(r) => this.flippy = r} // to use toggle method like this.flippy.toggle()
-        // if you pass isFlipped prop component will be controlled component.
-        // and other props, which will go to div
-        style={{ position: 'relative',width: '500px', height: '60px', color: 'white', marginBottom: '30px' }} /// these are optional style, it is not necessary
-      >
-        <FrontSide
-          style={{
-            backgroundColor: '#603348',
-          }}
+
+
+
+    render() {
+
+      return (
+          /*
+        <Flippy
+          flipOnHover={false} // default false
+          flipOnClick={true} // default false
+          flipDirection="horizontal" // horizontal or vertical
+          ref={(r) => this.flippy = r} // to use toggle method like this.flippy.toggle()
+          // if you pass isFlipped prop component will be controlled component.
+          // and other props, which will go to div
+          style={{ position: 'relative',width: '500px', height: '120px', color: 'white', marginBottom: '30px', fontsize: '500px'}} /// these are optional style, it is not necessary
         >
-          {this.props.question}
-        </FrontSide>
-        <BackSide
-          style={{ width: '300px', height: '300px', backgroundColor: '#603348', clear: 'both'}}>
-          {this.props.answer}
-        </BackSide>
-      </Flippy>
+          <FrontSide
+            style={{
+              position: 'relative',width: '500px', height: '60px', color: 'white', fontsize: '500px',
+              backgroundColor: '#603348',
+            }}
+          >
+            <p style={{fontSize: '40px'}}> {this.props.question}</p>
+          </FrontSide>
+          <BackSide
+            style={{
+              backgroundColor: '#603348'}}>
+            <p style={{fontSize: '40px'}}> {this.props.answer}</p>
+          </BackSide>
+        </Flippy>
+          */
 
 
-    );
-  }
+
+          <button className={"answer"} onClick={this.handleClick}>
+                <div>
+                  <p className={'questionText'}>{this.props.question}</p>
+              <p className={'answerText'}>{this.props.answer}</p>
+            </div>
+          </button>
+
+
+      );
+    }
+
+
+
+
 }
